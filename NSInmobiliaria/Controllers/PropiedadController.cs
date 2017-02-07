c96741d83f705f8be4b1b22863f35b72d1ba9da1
EvaristoM <EvaristoM@DES010>
2017-02-06 21:36:08 -0300

    Add project files.

---
+++ NSInmobiliaria/Controllers/PropiedadController.cs
@@ -0,0 +1,28 @@
+﻿using NSInmobiliaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSInmobiliaria.Controllers
+{
+    public class PropiedadController : Controller
+    {
+        // GET: Propiedad
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PropiedadViewModel model)
+        {
+            var propiedad = new PropiedadViewModel(model.Direccion, model.Precio);
+            return View();
+        }
+    }
+}
---
+++ NSInmobiliaria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NSInmobiliaria.MvcApplication" Language="C#" %>

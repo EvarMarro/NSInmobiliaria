b8a68e5bae1dfa6a55f7ccc5e1361350d43d04e6
Evar <jose.evaristo.marro@gmail.com>
2017-02-06 21:48:05 -0300

    Se agrega el atributo ID al modelo de la propiedad

--- NSInmobiliaria/Models/PropiedadViewModel.cs
+++ NSInmobiliaria/Models/PropiedadViewModel.cs
@@ -17,5 +17,6 @@ namespace NSInmobiliaria.Models
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Direccion { get; private set; }
         public double Precio { get; private set; }
+        public int Id { get; private set; }
     }
 }
